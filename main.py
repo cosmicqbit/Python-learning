2164ad687b667f6eb96027b05297ef9e492f2b49
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-04-21 23:38:55 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello World")
+# name = input("What's your name? \n")
+# print("Hello! " + name + " Welcome to the world of idiots ")
+
+# Arithmetic Operations
+print(3 / 2 ) # Division
+print(3 + 8) # Addition
+print(2 ** 3) # Exponent
+print(5 // 2) # Floor Division
+print(4 % 3) # Modulus Division
+
