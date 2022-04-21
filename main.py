ac7b0215c9804b25e66bf6e127f6122c943e7c59
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-04-21 23:42:47 +0530

    Learning Comparison operators

--- main.py
+++ main.py
@@ -8,4 +8,27 @@ print(3 + 8) # Addition
 print(2 ** 3) # Exponent
 print(5 // 2) # Floor Division
 print(4 % 3) # Modulus Division
+# for a%b => a = q * b + remainder
+
+# Complement Assignments 
+num = 5 
+num = num + 2
+num += 2
+
+#Absolute value
+print(abs(-2)) # will return 
+
+#Round value 
+print(round(2.4)) # will return 2 
+print(round(2.334, 1)) # round off to 2 decimal place
+
+#Comparisons 
+
+3 == 2
+2 != 23
+3 > 2
+2 <  1
+4 >= 2
+2 <= 4
+
 
