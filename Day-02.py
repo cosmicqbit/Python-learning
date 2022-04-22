c1ce0769a9d13334923eb1b180ad10e959c83d44
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-04-22 23:26:47 +0530

    Learning Append & Insert Method on Lists

--- Day-02.py
+++ Day-02.py
@@ -4,4 +4,13 @@
 # Array 
 courses = ['coa', 'os', 'dsa', 'dbms']
 print(courses)
+print(len(courses)) # Returns the length
+print(courses[0:3])
 
+# Append -> Method to add element at the end
+courses.append('probability')
+print(courses)
+
+# Insert -> Inserts elemetn at a specific index
+courses.insert(0,  'architecture')
+print(courses)
