058935c274bfd0ee9d983dac1294604e224eb0e4
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-04-23 20:34:06 +0530

    Learning elif

--- Day-03.py
+++ Day-03.py
@@ -1,13 +1,29 @@
 # Condtions & Loops
 
 # If
-if False:
-    print("This line is dead, it will never get printed")
+num = 10
+if num % 2 == 0:
+    print('Num is an even number')
 
 # If else
 if True: 
     print('This is the new line')
 else:
     print("This line won't execute")
+
+num = 15 
+if num % 2 == 0:
+    print('The number is even')
+else:
+    print('The number is odd')
+
+# else if
+city = 'Chennai'
+if city == 'Bangalore':
+    print('City is Bangalore')
+elif city == 'Chennai':
+    print('City is Chennai')
+else:
+    print('Not matching')
 
 
