19d29e95cf714b931d9e0b29861992c9a0c67d00
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-04-23 20:29:36 +0530

    Learning if, if else

---
+++ Day-03.py
@@ -0,0 +1,13 @@
+# Condtions & Loops
+
+# If
+if False:
+    print("This line is dead, it will never get printed")
+
+# If else
+if True: 
+    print('This is the new line')
+else:
+    print("This line won't execute")
+
+
