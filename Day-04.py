a59a39d2962964e2fbe625d5f1f5e94eb3222f9f
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-04-24 23:40:22 +0530

    Updagte Day-04

--- Day-04.py
+++ Day-04.py
@@ -67,5 +67,11 @@ courses = ['COA', 'OS']
 info = {'name': 'Shariq', 'age':20}
 func(*courses, **info)
 
+# Checking leap year
+month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+def is_leap(year):
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
 
 
