a91c29efb366834a0b99a1d3c2eb852a3630f776
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-04-24 23:44:35 +0530

    Update Day-04

--- Day-04.py
+++ Day-04.py
@@ -67,11 +67,25 @@ courses = ['COA', 'OS']
 info = {'name': 'Shariq', 'age':20}
 func(*courses, **info)
 
-# Checking leap year
+# Checking leap year #
+# -------------- #
+
 month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 def is_leap(year):
+    # Return True for leap years, and False for non-leap years.
     return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
+def days_in_month(year, month):
+    # Return no of days in that month in that year.
+    if not 1 <= month <= 12:
+        return 'Invalid Month'
 
+    if month == 2 and is_leap(year):
+        return 29
+
+    return month_days[month]
+
+print(is_leap(2024))
+print(days_in_month(2024, 2))
 
