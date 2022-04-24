23dcb50fa29e06f116bddb30f9584a29bdd86416
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-04-24 23:01:06 +0530

    Returning a value via function

--- Day-04.py
+++ Day-04.py
@@ -4,3 +4,16 @@ def func():
     pass # Not doing anything
 
 func()
+
+print(func) # Will return reference for the function
+
+# /// ----- ///
+
+def hello_func():
+    return 'Hello people over the Internet'
+
+hello_func() # Won't display any result 
+print(hello_func()) # This will print the string in the output
+string = hello_func() 
+print(string) # Same as above
+
