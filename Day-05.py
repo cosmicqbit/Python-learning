ca92f2614fb2c4459e9c8795915b1a5d5dd9b2cf
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-04-25 23:06:26 +0530

    Positive & Negative Indexes

--- Day-05.py
+++ Day-05.py
@@ -5,3 +5,11 @@
 
 # Slicing --> Extracting certain elements from list or string
 
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#          0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+#        -10,-9,-8,-7,-6,-5,-4,-3,-2,-1 
+
+print(my_list[0]) # Will return 0
+print(my_list[-1]) # will return last element 
+
+# Negative indexes come handy when we don't know the length of List
