91699bd644c3d2073df3ad44560f9fba45f0b19e
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-04-26 10:39:22 +0530

    Printing File Name & checking file mode

--- Day-06.py
+++ Day-06.py
@@ -3,3 +3,17 @@
 # Files --> Named collection of records stored into the secondary memory
 # File characteristics --> Name + Extension
 
+content = open('payload.txt', 'r')
+content.close() # Have to Explicitly close it
+
+# "r" - Read : Default value. Opens file for reading
+# "a" - Append : opens a file for appending, creates the file if it does not exist
+# "w" - Write : Opens a file for writing, creates the file if it does not exist
+# "x" - Create : Creates the specified file, returns an error if the it does not 
+
+# Printing the file name
+print(content.name)
+
+# Check the file mode
+print(content.mode) # In the case will return 'r' : reading mode
+
