7631a9f8438dbda60ee9efa9a85a8bfd7cee4a04
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-04-30 23:56:24 +0530

    Add Board Layout

--- Tic-Tac-Toe/main.py
+++ Tic-Tac-Toe/main.py
@@ -7,8 +7,17 @@ def printBoard():
 
 if __name__ == "__main__":
     xState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    yState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    zState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     turn = 1 # 1 for X & 0 for O
     print("Welcome to Tic Tac Toe Game!")
-    print("X's Chance")
-    printBoard()
+    while(True):
+        printBoard()
+        if(turn == 1):
+            print("X's Chance")
+            value = int(input("Please enter a value\n"))
+            xState[value] = 1
+        else:
+            print("O's Chance")
+            value = int(input("Please enter a value\n"))
+            zState[value] = 1
+        break
