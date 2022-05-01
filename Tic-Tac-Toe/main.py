0207d5053ac7af156e6109175afe4cc34db6e4a9
cosmicqbit <shariqrazaqadri@protonmail.com>
2022-05-01 11:04:28 +0530

    Update main.py

--- Tic-Tac-Toe/main.py
+++ Tic-Tac-Toe/main.py
@@ -1,9 +1,19 @@
-def printBoard():
-    print(f"0 | 1 | 2 |")
+def printBoard(xState, zState):
+    zero = 'X' if xState[0] else ('0' if zState[0] else 0)
+    one = 'X' if xState[1] else ('0' if zState[1] else 1)
+    two = 'X' if xState[2] else ('0' if zState[2] else 2)
+    three = 'X' if xState[3] else ('0' if zState[3] else 3)
+    four = 'X' if xState[4] else ('0' if zState[4] else 4)
+    five = 'X' if xState[5] else ('0' if zState[5] else 5)
+    six = 'X' if xState[6] else ('0' if zState[6] else 6)
+    seven = 'X' if xState[7] else ('0' if zState[7] else 7)
+    eight = 'X' if xState[8] else ('0' if zState[8] else 8)
+
+    print(f"{zero} | {one} | {two} |")
     print(f"--|---|---|")
-    print(f"3 | 4 | 5")
+    print(f"{three} | {four} | {five}")
     print(f"--|---|---|")
-    print(f"6 | 7 | 8 |")
+    print(f"{six} | {seven} | {eight} |")
 
 if __name__ == "__main__":
     xState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -11,7 +21,7 @@ if __name__ == "__main__":
     turn = 1 # 1 for X & 0 for O
     print("Welcome to Tic Tac Toe Game!")
     while(True):
-        printBoard()
+        printBoard(xState, zState)
         if(turn == 1):
             print("X's Chance")
             value = int(input("Please enter a value\n"))
@@ -20,4 +30,4 @@ if __name__ == "__main__":
             print("O's Chance")
             value = int(input("Please enter a value\n"))
             zState[value] = 1
-        break
+        turn = 1 - turn
